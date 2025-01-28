d3e7d7e5051b4c69c5f4b36a9c403d1a2da50fc0
mp-09 <aditya_bhadauriya@pibmindore.ac.in>
2025-01-28 21:26:12 +0530

    util code

--- src/index.js
+++ src/index.js
@@ -6,26 +6,13 @@ dotenv.config({
     path: './env'
 })
 connectDB()
-
-/*
-import express from "express";
-const app = express()
-
-( async() => {
-    try{    
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        app.on("error", (error) => {
-            console.log("ERROR: ", error);
-            throw error
-        })
-        app.listen(process.env.PORT, () => {
-            console.log(`App is listening on port ${process.env.PORT}`);
-            
-        });
-    }catch(error){
-        console.error("ERROR:", error)
-        throw err
-    }
-})()
-
-*/
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is runnig ar port: ${process.env.PORT}`);
+        
+    })
+})
+.catch((err) => {
+    console.log("Mongo db connection failed !!!", err);
+    
+})
